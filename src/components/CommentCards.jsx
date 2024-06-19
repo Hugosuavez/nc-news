94e54d5a33eedfa77a749ac671cdf0709915d3b7
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2024-06-19 10:37:09 +0100

    add optimistic render of comments without deleted comment

--- src/components/CommentCards.jsx
+++ src/components/CommentCards.jsx
@@ -1,9 +1,9 @@
 import { DeleteButton } from "./DeleteButton"
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { UserContext } from './UserContext'
 
-export const CommentCards = ({comments}) => {
-
+export const CommentCards = ({comments, setComments}) => {
+   
     const {username} = useContext(UserContext)
 
     return comments.map((comment) => {
@@ -12,7 +12,7 @@ export const CommentCards = ({comments}) => {
             <h4 id="comment-title">{comment.author} | {date}</h4>
             <p>{comment.body}</p>
             <button>Votes: {comment.votes}</button>
-            {comment.author === username ? <DeleteButton comment_id={comment.comment_id}/> : null} 
+            {comment.author === username ? <DeleteButton comment_id={comment.comment_id} setComments={setComments} comments={comments}/> : null} 
         </li>)
     })
 }
--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react"
+
 import { deleteComment } from "../utils/apicalls"
 
 
-export const DeleteButton = ({comment_id}) => {
-    
-    
+export const DeleteButton = ({comment_id, comments, setComments}) => {
+   
     const handleDelete = () => {
+        const newComments = comments.filter((comment) => {
+            return comment.comment_id !== comment_id
+        })
+        setComments(newComments)
         deleteComment(comment_id)
-    
     }
-
+ 
+    
 
     return <button onClick={handleDelete}>Delete Comment</button>
 }
