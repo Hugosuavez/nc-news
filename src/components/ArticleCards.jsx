00cee7651b0994613b3b99d9a6caa9b9e742b3be
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2024-06-21 11:39:20 +0100

    restyle article cards

--- src/components/ArticleCards.jsx
+++ src/components/ArticleCards.jsx
@@ -1,19 +1,15 @@
 import { Link } from "react-router-dom";
 
 export const ArticleCards = ({ articles }) => {
-  return articles.map((article, index) => {
+  return articles.map((article) => {
     const date = new Date(article.created_at).toDateString();
     return (
       <article className={`article-card`} key={article.article_id}>
-        <h3 className="topic">{article.topic}</h3>
-        <h2>{article.title} </h2>
-        <h3>by {article.author}</h3>
         <img className="article-image" src={article.article_img_url} alt="" />
-        <br />
-        <p>{date}</p>
-        <button>Votes: {article.votes}</button>
-        <button>Comments: {article.comment_count}</button>
-        <Link to={`/api/articles/${article.article_id}`}>View Article</Link>
+        <h2 className="article-header">{article.title} </h2>
+        <p className="article-author">{article.author} | {article.topic} | {date}</p>
+        <p></p>
+        <Link className="nav-button" to={`/api/articles/${article.article_id}`}>View Article</Link>
       </article>
     );
   });
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,7 @@ export const Header = () => {
     const {username} = useContext(UserContext)
     
     return (<section className='header'>
+    <button className='userButton'>User: {username}</button>
     <h1>NC News</h1>
-    <button>+</button>
-    <button>User: {username}</button>
     </section>)
 }
