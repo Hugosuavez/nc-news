447c947d3aa73769deb8c220d9c3b01d9af15f69
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2024-07-26 12:24:21 +0100

    store vote status in localstorage

--- src/components/UpdateVotes.jsx
+++ src/components/UpdateVotes.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { updateArticleVotes } from "../utils/apicalls";
 import { useContext } from "react";
 import { UserContext } from "./UserContext";
@@ -11,7 +11,16 @@ export const UpdateVotes = ({ article }) => {
   const [err, setErr] = useState(null);
   const [userVoted, setUserVoted] = useState();
 
-  const handleVote = (event) => {
+  useEffect(() => {
+    const storedVoteStatus = localStorage.getItem(
+      `${username}_${article.article_id}_liked`
+    );
+    if (storedVoteStatus) {
+      setUserVoted(JSON.parse(storedVoteStatus));
+    }
+  }, [username, article.article_id]);
+
+  const handleVote = () => {
     if (username) {
       let inc_votes = 1;
       if (userVoted) {
@@ -23,11 +32,18 @@ export const UpdateVotes = ({ article }) => {
         inc_votes = 1;
       }
 
+      const newVoteStatus = !userVoted;
+      setUserVoted(newVoteStatus);
+
       setVotes((currentCount) => currentCount + inc_votes);
 
       updateArticleVotes(article.article_id, inc_votes)
         .then(() => {
           setErr(null);
+          localStorage.setItem(
+            `${username}_${article.article_id}_liked`,
+            JSON.stringify(newVoteStatus)
+          );
         })
         .catch((err) => {
           setUserVoted((current) => !current);
@@ -37,11 +53,12 @@ export const UpdateVotes = ({ article }) => {
     } else setErr("Please log in to vote");
   };
 
- 
-  if(article.author === username){
-    return (<>
+  if (article.author === username) {
+    return (
+      <>
         <p className="vote-tag">Votes: {votes}</p>
-    </>)
+      </>
+    );
   }
   return (
     <>
